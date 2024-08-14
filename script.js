d7b3004d6e7f0081363b559c57e01166b25121bd
Shivanshu Mishra <163833644+shiva6307@users.noreply.github.com>
2024-08-14 11:21:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,18 @@ window.addEventListener('scroll', function() {
     }
 });
 
-    function toggleMenu() {
-        const navLinks = document.querySelector('.nav-links');
-        navLinks.classList.toggle('active');
-    }
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
 
-    window.addEventListener('scroll', () => {
-        const navbar = document.querySelector('.navbar');
-        if (window.scrollY > 50) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
+// Scroll event listener to add and remove the 'scrolled' class based on scroll position
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
