b85a2218b51496f79d374b6ccfc40f8f0f1ed274
Shivanshu Mishra <163833644+shiva6307@users.noreply.github.com>
2024-08-15 15:33:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,18 +10,3 @@ window.addEventListener('scroll', function() {
     }
 });
 
-function toggleMenu() {
-    var navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
-
-// Scroll event listener to add and remove the 'scrolled' class based on scroll position
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
-
